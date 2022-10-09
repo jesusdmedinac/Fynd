a4e5e3d1359f981da7f46671ce5ae813d8000676
Jesús Daniel Medina Cruz <jesus.daniel.medina.cruz@gmail.com>
2022-10-08 17:07:12 -0700

    Columns and rows implemented

--- app/src/main/java/com/jesusdmedinac/fynd/ui/FyndApp.kt
+++ app/src/main/java/com/jesusdmedinac/fynd/ui/FyndApp.kt
@@ -1,16 +1,20 @@
 package com.jesusdmedinac.fynd.ui
 
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Error
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -23,15 +27,36 @@ import com.jesusdmedinac.fynd.viewmodel.FyndAppViewModel
 fun FyndApp() {
     val fyndAppViewModel: FyndAppViewModel = hiltViewModel()
 
+    val fyndAppState by fyndAppViewModel.container.stateFlow.collectAsState()
+    val fyndAppSideEffect by fyndAppViewModel
+        .container
+        .sideEffectFlow
+        .collectAsState(initial = FyndAppViewModel.SideEffect.Idle)
+
+    val context = LocalContext.current
+
+    LaunchedEffect(fyndAppSideEffect) {
+        when (fyndAppSideEffect) {
+            FyndAppViewModel.SideEffect.ColumnsLimitReached -> {
+                Toast.makeText(context, "Intenta entre 0 y 10 columnas", Toast.LENGTH_SHORT).show()
+            }
+            FyndAppViewModel.SideEffect.RowsLimitReached -> {
+                Toast.makeText(context, "Intenta entre 0 y 26 filas", Toast.LENGTH_SHORT).show()
+            }
+            else -> Unit
+        }
+    }
+
     Scaffold(modifier = Modifier.padding(16.dp)) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(it)
         ) {
-            val fyndAppState by fyndAppViewModel.container.stateFlow.collectAsState()
             var columnsText = fyndAppState.columnsText
             var rowsText = fyndAppState.rowsText
+            val invalidColumnsLimit = fyndAppState.invalidColumnsLimit
+            val invalidRowsLimit = fyndAppState.invalidRowsLimit
 
             Row(
                 modifier = Modifier
@@ -43,10 +68,23 @@ fun FyndApp() {
                         fyndAppViewModel.onColumnsValueChange(value)
                     },
                     placeholder = {
-                        Text(text = "Columns")
+                        Text(text = "Columnas")
                     },
                     keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                    modifier = Modifier.weight(1f)
+                    modifier = Modifier.weight(1f),
+                    isError = invalidColumnsLimit,
+                    trailingIcon = {
+                        if (invalidColumnsLimit) {
+                            Icon(
+                                Icons.Filled.Error,
+                                "error",
+                                tint = MaterialTheme.colorScheme.error
+                            )
+                        }
+                    },
+                    label = {
+                        Text("Columnas")
+                    },
                 )
 
                 Spacer(modifier = Modifier.width(8.dp))
@@ -57,55 +95,86 @@ fun FyndApp() {
                         fyndAppViewModel.onRowsValueChange(value)
                     },
                     placeholder = {
-                        Text(text = "Rows")
+                        Text(text = "Filas")
                     },
                     keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                    modifier = Modifier.weight(1f)
+                    modifier = Modifier.weight(1f),
+                    isError = invalidRowsLimit,
+                    trailingIcon = {
+                        if (invalidRowsLimit) {
+                            Icon(
+                                Icons.Filled.Error,
+                                "error",
+                                tint = MaterialTheme.colorScheme.error
+                            )
+                        }
+                    },
+                    label = {
+                        Text("Filas")
+                    },
                 )
             }
 
             val columns = if (fyndAppState.columns == 0) 1
             else fyndAppState.columns
-            val rows = fyndAppState.rows
             val total = fyndAppState.total
 
-            LazyVerticalGrid(
-                columns = GridCells.Fixed(columns),
-                horizontalArrangement = Arrangement.spacedBy(8.dp),
-                verticalArrangement = Arrangement.spacedBy(8.dp),
-                modifier = Modifier.padding(16.dp)
-            ) {
-                items(total) { cell ->
-                    val isColumnHeader = cell in 0 until columns
-                    val cellBetweenColumns = cell.toDouble() / columns.toDouble()
-                    val isRowHeader = cellBetweenColumns % 1 == 0.0
-                    if (cell == 0) return@items
-                    if (isColumnHeader || isRowHeader) {
-                        Box(
-                            modifier = Modifier
-                                .size(42.dp)
-                                .clip(RoundedCornerShape(16.dp)),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            val alphabetLetters = ('a'..'z').toList()
-                            val cellText =
-                                if (isRowHeader) alphabetLetters[cellBetweenColumns.toInt() - 1]
-                                    .toString()
-                                    .uppercase()
-                                else cell.toString()
+            when {
+                invalidColumnsLimit || invalidRowsLimit -> {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                    ) {
+                        if (invalidColumnsLimit) {
                             Text(
-                                text = cellText,
-                                color = MaterialTheme.colorScheme.onBackground
+                                text = "Intenta entre 0 y 10 columnas",
+                                color = MaterialTheme.colorScheme.error,
                             )
                         }
-                    } else {
-                        Button(
-                            modifier = Modifier
-                                .clip(RoundedCornerShape(16.dp))
-                                .size(42.dp)
-                                .background(MaterialTheme.colorScheme.primary),
-                            onClick = {}
-                        ) {}
+                        if (invalidRowsLimit) {
+                            Text(
+                                text = "Intenta entre 0 y 26 filas",
+                                color = MaterialTheme.colorScheme.error,
+                            )
+                        }
+                    }
+                }
+                else -> {
+                    LazyVerticalGrid(
+                        columns = GridCells.Fixed(columns),
+                        horizontalArrangement = Arrangement.spacedBy(8.dp),
+                        verticalArrangement = Arrangement.spacedBy(8.dp),
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+                        items(total) { cell ->
+                            val isColumnHeader = cell in 0 until columns
+                            val cellBetweenColumns = cell.toDouble() / columns.toDouble()
+                            val isRowHeader = cellBetweenColumns % 1 == 0.0
+                            if (cell == 0) return@items
+                            if (isColumnHeader || isRowHeader) {
+                                Box(
+                                    modifier = Modifier
+                                        .size(42.dp)
+                                        .clip(RoundedCornerShape(16.dp)),
+                                    contentAlignment = Alignment.Center
+                                ) {
+                                    val cellText = fyndAppViewModel.cellText(cell, columns)
+                                    Text(
+                                        text = cellText,
+                                        color = MaterialTheme.colorScheme.onBackground
+                                    )
+                                }
+                            } else {
+                                Button(
+                                    modifier = Modifier
+                                        .clip(RoundedCornerShape(16.dp))
+                                        .size(42.dp)
+                                        .background(MaterialTheme.colorScheme.primary),
+                                    onClick = {}
+                                ) {}
+                            }
+                        }
                     }
                 }
             }
--- app/src/main/java/com/jesusdmedinac/fynd/viewmodel/FyndAppViewModel.kt
+++ app/src/main/java/com/jesusdmedinac/fynd/viewmodel/FyndAppViewModel.kt
@@ -3,10 +3,13 @@ package com.jesusdmedinac.fynd.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import org.orbitmvi.orbit.Container
 import org.orbitmvi.orbit.ContainerHost
 import org.orbitmvi.orbit.container
+import org.orbitmvi.orbit.syntax.simple.SimpleSyntax
 import org.orbitmvi.orbit.syntax.simple.intent
+import org.orbitmvi.orbit.syntax.simple.postSideEffect
 import org.orbitmvi.orbit.syntax.simple.reduce
 import javax.inject.Inject
 
@@ -17,15 +20,39 @@ class FyndAppViewModel @Inject constructor() :
     override val container: Container<State, SideEffect> =
         viewModelScope.container(State())
 
-    fun onColumnsValueChange(value: String) {
+    fun onColumnsValueChange(textValue: String) {
         intent {
-            reduce { state.copy(columnsText = value) }
+            val value = textValue.toIntOrNull() ?: 0
+            val invalidColumnsLimit = if (value !in COLUMNS_SIZE_RANGE) {
+                workAroundPostSideEffect(SideEffect.ColumnsLimitReached)
+                true
+            } else {
+                false
+            }
+            reduce {
+                state.copy(
+                    columnsText = textValue,
+                    invalidColumnsLimit = invalidColumnsLimit,
+                )
+            }
         }
     }
 
-    fun onRowsValueChange(value: String) {
+    fun onRowsValueChange(textValue: String) {
         intent {
-            reduce { state.copy(rowsText = value) }
+            val value = textValue.toIntOrNull() ?: 0
+            val invalidRowsLimit = if (value !in ROWS_SIZE_RANGE) {
+                workAroundPostSideEffect(SideEffect.RowsLimitReached)
+                true
+            } else {
+                false
+            }
+            reduce {
+                state.copy(
+                    rowsText = textValue,
+                    invalidRowsLimit = invalidRowsLimit,
+                )
+            }
         }
     }
 
@@ -35,16 +62,33 @@ class FyndAppViewModel @Inject constructor() :
         }
     }
 
+    fun cellText(cell: Int, columns: Int): String {
+        val cellBetweenColumns = cell.toDouble() / columns.toDouble()
+        val isRowHeader = cellBetweenColumns % 1 == 0.0
+        val alphabetLetters = ('a'..'z').toList()
+        val amountOfAlphabetLetters = alphabetLetters.size
+        val alphabetLetterCalculatedIndex = cellBetweenColumns.toInt() - 1
+        val alphabetLetterIndex = alphabetLetterCalculatedIndex % amountOfAlphabetLetters
+
+        return when {
+            isRowHeader -> alphabetLetters[alphabetLetterIndex]
+                .toString()
+                .uppercase()
+            else -> cell.toString()
+        }
+    }
+
     data class State(
         val columnsText: String = "1",
         val rowsText: String = "1",
+        val invalidColumnsLimit: Boolean = false,
+        val invalidRowsLimit: Boolean = false,
     ) {
         val total: Int
             get() = if (columnsText.isEmpty() || rowsText.isEmpty()) 0
             else {
                 columns * rows
             }
-
         val columns: Int
             get() = if (columnsText.isEmpty()) 0
             else {
@@ -56,7 +100,23 @@ class FyndAppViewModel @Inject constructor() :
             else {
                 rowsText.toInt() + 1
             }
+
     }
 
-    sealed class SideEffect
+    private suspend fun SimpleSyntax<State, SideEffect>.workAroundPostSideEffect(sideEffect: SideEffect) {
+        postSideEffect(sideEffect)
+        delay(100)
+        postSideEffect(SideEffect.Idle)
+    }
+
+    sealed class SideEffect {
+        object Idle : SideEffect()
+        object ColumnsLimitReached : SideEffect()
+        object RowsLimitReached : SideEffect()
+    }
+
+    companion object {
+        private val COLUMNS_SIZE_RANGE = 0..10
+        private val ROWS_SIZE_RANGE = 0..26
+    }
 }
